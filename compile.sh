1715024940f794c21d6b7fa7a500ef69a82f2112
Andrzej Broński <andrzej1_1@o2.pl>
2020-04-24 08:14:33 +0200

    Add robot boilerplate

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+if ([ -h "${SCRIPT_PATH}" ]); then
+  while([ -h "${SCRIPT_PATH}" ]); do cd `dirname "$SCRIPT_PATH"`; 
+  SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+export CLASSPATH=/usr/lib/jvm/java-8-openjdk/lib
+
+echo "Compiling ANDBRO robot..."
+javac -classpath ${SCRIPT_PATH}/robocode/libs/robocode.jar ${SCRIPT_PATH}/my-robot/QLearningRobot.java
+
+echo "Copying robots..."
+mkdir -p ${SCRIPT_PATH}/robocode/robots/iwium
+cp ${SCRIPT_PATH}/my-robot/QLearningRobot.class ${SCRIPT_PATH}/robocode/robots/iwium/
+
+echo "Done!"
