2a2f9b533482b543c3bbcd080b925f13329cb5ea
Andrzej Broński <andrzej1_1@o2.pl>
2020-04-24 08:06:32 +0200

    Robocode boilerplate

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+if ([ -h "${SCRIPT_PATH}" ]); then
+  while([ -h "${SCRIPT_PATH}" ]); do cd `dirname "$SCRIPT_PATH"`; 
+  SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+(
+  cd ${SCRIPT_PATH}/robocode
+  java -jar ./libs/robocode.jar
+)
