4df06da5fe47326966b5472338a2af2c633e6356
Andrzej Broński <andrzej1_1@o2.pl>
2020-05-24 14:50:13 +0200

    Now util for plotting rewards can save charts to files

--- utils/plot_stats.py
+++ utils/plot_stats.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 import sys
 
-train_rounds = int(sys.argv[1])
-test_rounds = int(sys.argv[2])
+parser = argparse.ArgumentParser(description='Prints winning statistics of logs')
+parser.add_argument('num_learning_rounds', type=int,
+                    help='number of learning rounds')
+parser.add_argument('num_testing_rounds', type=int,
+                    help='number of testing rounds')
+parser.add_argument('--save_rewards', action='store', help='Filename to save reward charts')
+parser.add_argument('--save_states', action='store', help='Filename to save states charts')
+args = parser.parse_args()
+
+train_rounds = args.num_learning_rounds
+test_rounds = args.num_testing_rounds
 ma_window = int(test_rounds/2)
 
 print('Plotting rewards...')
@@ -15,7 +25,10 @@ plt.plot(rewards_df['val'][-test_rounds:].rolling(ma_window).mean(), label='Test
 plt.legend(loc='best')
 plt.suptitle('')
 plt.title('Robocode AI - MA of rewards\nWindow size: ' + str(ma_window) + ' rounds')
-plt.show()
+if args.save_rewards:
+  plt.savefig(args.save_rewards)
+else:
+  plt.show()
 
 print('Plotting states...')
 states_df = pd.read_csv('../logs/states.txt', sep=",", header=None)
@@ -26,4 +39,7 @@ plt.plot(states_df['val'][-test_rounds:], label='Testing')
 plt.legend(loc='best')
 plt.suptitle('')
 plt.title('Robocode AI - number of explored states')
-plt.show()
+if args.save_states:
+  plt.savefig(args.save_states)
+else:
+  plt.show()
